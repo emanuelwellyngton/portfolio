8f2ac15057a21349ff534fb7a2bdaa58485c730f
Emanuel <emanuelwellygton@gmail.com>
2025-12-21 12:34:27 -0300

    refactor: consuming the website name dynamically, defining the pages titles and changing the blog rout

--- app.routes.ts
+++ app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './app/pages/home/home';
 import { BlogPostComponent } from './app/components/blog-post/blog-post.component';
+import { BlogComponent } from './app/components/blog/blog.component';
+
+const websiteTitle = "wellyngton.dev";
 
 export const AppRoutes: Routes = [
     {
         path: "",
+        title: "Home | " + websiteTitle,
         component: Home
     },
     {
-        path: "post",
-        component: BlogPostComponent
+        path: "blog",
+        title: "Blog | " + websiteTitle,
+        component: BlogComponent
     }
 ];
