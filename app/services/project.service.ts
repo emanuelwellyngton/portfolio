e2e308b68cc63533d40ddc6d6934976872231d96
Emanuel <emanuelwellygton@gmail.com>
2025-12-29 18:37:22 -0300

    feat: use environment variable for project service API URL

--- app/services/project.service.ts
+++ app/services/project.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Project } from '../models/project.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProjectService {
     private http = inject(HttpClient);
-    private apiUrl = 'http://localhost:8080/projects';
+    private apiUrl = environment.apiUrl + '/projects';
 
     getProjects(): Observable<Project[]> {
         return this.http.get<Project[]>(this.apiUrl);
